2833bf0bc045581b5ecd612eeb1823eb18f6b460
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2023-01-13 02:59:24 +0600

    Update RIDOY.py

--- RIDOY.py
+++ RIDOY.py
@@ -1,6 +1,6 @@
 import os,platform
 os.system('clear')
-print('[•] Checking Updates...')
+print('\033[1;34m[•] Checking Updates...')
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
