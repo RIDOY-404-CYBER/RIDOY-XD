44899fc84f4b0c88c226066bd656d12f9036dc12
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2023-01-13 02:29:06 +0600

    Update Xxx.py

--- Xxx.py
+++ Xxx.py
@@ -1,13 +1,25 @@
 import os
+
 os.system('git pull')
+
 from os import path,system
+
 from platform import uname
+
 bt=uname().machine.lower()
+
 if 'aarch' in bt:
+
     print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
+
     if path.isfile("RMX3.cpython-311.so"):
+
         pass
+
     else:
-        system("curl -L https://github.com/RIDOY-404-CYBER/RIDOY-XD/blob/main/RMX3 -o RMX3")
+
+        system("curl -L https://github.com/RIDOY-404-CYBER/RIDOY-XD/blob/main/RMX3.cpython-311.so -o RMX3.cpython-311.so")
+
 else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
-os.system('chmod 777 RMX3 && ./RMX3')
+
+os.system('chmod 777 RMX3.cpython-311.so && ./RMX3.cpython-311.so')
